367d5b25b35e3967b38ed928820960a1ec520700
Gugusteh <17476911+Gugusteh@users.noreply.github.com>
2022-09-17 01:04:57 +0200

    categories & comments init

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,19 +5,44 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = \Faker\Factory::create('fr_FR');
+        // categories
         for($i = 1; $i <= 3; $i++) {
-            $article = new Article();
-            $article->setTitle("Titre de l'article #$i")
-                    ->setContent("<p>Contenu de l'article #$i</p>")
-                    ->setImage("https://www.fillmurray.com/240/160")
-                    ->setCreatedAt(new \DateTimeImmutable())
-                    ->setAuthor("Gugusteh");
-            $manager->persist($article);
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+            $manager->persist($category);
+            //articles
+            for($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Article();
+                $article->setTitle($faker->sentence())
+                        ->setContent($faker->paragraphs(5, true))
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt((new \DateTimeImmutable())->createFromMutable($faker->dateTimeBetween('-6 months')))
+                        ->setAuthor("Gugusteh")
+                        ->setCategory($category);
+
+                $manager->persist($article);
+            
+                for($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+                    $days = (new \Datetime())->diff($article->getCreatedAt())->days;
+                    $comment->setAuthor($faker->name)
+                            ->setContent($faker->paragraphs(2, true))
+                            ->setCreatedAt((new \DateTimeImmutable())->createFromMutable($faker->dateTimeBetween('-'.$days.' days')))
+                            ->setArticle($article);
+                    
+                    $manager->persist($comment);
+                }
+            }
+    
         }
 
         $manager->flush();
